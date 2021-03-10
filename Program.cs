8afe25116f5a195539dd0dd5d4e84ad38676359d
Joel Sosa <choch2727@gmail.com>
2021-03-10 13:07:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeFirst
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        //public Author Author { get; set; }
+        //public IList<Tag> Tags { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
